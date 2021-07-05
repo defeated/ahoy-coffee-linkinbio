fc27b30dfd36c7999df3d49ed36665ce3eb6f272
eddie cianci <defeated2k4@gmail.com>
2021-07-05 14:38:10 -0400

    basic links

---
+++ src/components/Link.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>
+    <a href="{{ link.url }}">
+      {{ link.title }}
+    </a>
+  </p>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  link: Object,
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
