41a1a38ca3ed6c0ef5195b1de02d8489b76f4ca3
eddie cianci <defeated2k4@gmail.com>
2021-07-14 16:48:22 -0400

    extract AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-gradient-to-t from-white to-yellow-200 h-40 pt-7 border-b-2 border-cyan-200">
+    <img src="@/assets/logo-outline-opt.svg" alt="Ahoy Coffee Co. logo"
+      class="h-16 w-16 mx-auto">
+  </div>
+</template>
