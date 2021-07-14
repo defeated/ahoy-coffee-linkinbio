1c60fb5b11ecbde97c1feffe90dfb79b74baf776
eddie cianci <defeated2k4@gmail.com>
2021-07-13 22:48:33 -0400

    extract link data to json file:
    https://vitejs.dev/guide/features.html#json

--- src/components/App.vue
+++ src/components/App.vue
@@ -11,6 +11,7 @@
 
 <script setup>
 import LinkItem from './LinkItem.vue'
+import links from '../data/link_items.json'
 import {
   GlobeAltIcon, DocumentTextIcon, CameraIcon, CreditCardIcon
 } from '@heroicons/vue/outline'
@@ -18,29 +19,6 @@ import {
 const icons = {
   GlobeAltIcon, DocumentTextIcon, CameraIcon, CreditCardIcon
 }
-
-const links = [
-  {
-    title: "Ahoy Coffee Co.",
-    url: "https://www.ahoy.coffee/",
-    icon: "GlobeAltIcon",
-  },
-  {
-    title: "Blog",
-    url: "https://www.ahoy.coffee/blogs/news/",
-    icon: "DocumentTextIcon",
-  },
-  {
-    title: "Instagram",
-    url: "https://instagram.com/coffee.ahoy/",
-    icon: "CameraIcon",
-  },
-  {
-    title: "Venmo",
-    url: "https://instagram.com/coffee.ahoy/",
-    icon: "CreditCardIcon",
-  },
-];
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
