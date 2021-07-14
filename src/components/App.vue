d31edd1c88d65c553a3312770faf56c88e742d50
eddie cianci <defeated2k4@gmail.com>
2021-07-14 16:51:58 -0400

    make links collection reactive

--- src/components/App.vue
+++ src/components/App.vue
@@ -12,8 +12,12 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import AppHeader from '@/components/AppHeader.vue'
 import LinkItem from '@/components/LinkItem.vue'
+import data from '@/data/LinkItems.json'
+
+const links = ref(data)
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
