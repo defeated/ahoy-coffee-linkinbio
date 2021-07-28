ddf335f65fcf0e01f3baf3a3814d911f8dc9804d
eddie cianci <defeated2k4@gmail.com>
2021-07-28 01:14:33 +0000

    add links and icons

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import { createApp } from 'vue'
 import App from '@/components/App.vue'
 import '@/main.css'
 import {
-  GlobeAltIcon, DocumentTextIcon, CameraIcon, CreditCardIcon
+  ShoppingCartIcon, DocumentTextIcon, CameraIcon, CreditCardIcon, NewspaperIcon
 } from '@heroicons/vue/outline'
 
 const app = createApp(App)
-app.component('GlobeAltIcon', GlobeAltIcon)
+app.component('ShoppingCartIcon', ShoppingCartIcon)
 app.component('DocumentTextIcon', DocumentTextIcon)
 app.component('CameraIcon', CameraIcon)
 app.component('CreditCardIcon', CreditCardIcon)
+app.component('NewspaperIcon', NewspaperIcon)
 app.mount('#app')
