fa8153e98795a16f8d4c0d67b97a8f582d034849
eddie cianci <defeated2k4@gmail.com>
2021-08-03 20:54:03 +0000

    ensure component test uses dev config

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,12 @@
 const { startDevServer } = require('@cypress/vite-dev-server')
+const path = require('path')
+const viteConfig = {
+  configFile: path.resolve(__dirname, '../../vite.config.js')
+}
 
 module.exports = (on, config) => {
   on('dev-server:start', (options) => {
-    return startDevServer({ options })
+    return startDevServer({ options, viteConfig })
   })
 
   return config
