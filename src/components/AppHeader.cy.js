a1e1a94ee590f5281b39427bbf0ed2f0c979c1ae
eddie cianci <defeated2k4@gmail.com>
2022-02-20 19:38:01 +0000

    link the header logo to homepage

--- src/components/AppHeader.cy.js
+++ src/components/AppHeader.cy.js
@@ -4,10 +4,16 @@ import AppHeader from '@/components/AppHeader.vue'
 describe('AppHeader', () => {
   it('renders the logo', () => {
     mount(AppHeader)
-      .get('header > img')
+      .get('header img')
       .should('be.visible')
       .and(($logo) => {
         expect($logo[0].naturalWidth).to.be.greaterThan(0)
       })
+  })
+
+  it('links the logo to homepage', () => {
+    mount(AppHeader)
+      .get('header a')
+      .should("have.attr", "href", "https://ahoy.coffee")
   })
 })
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,7 +4,8 @@
     bg-gradient-to-t from-white to-yellow-200
     border-b-2 border-cyan-200
   ">
-    <img src="@/assets/logo-outline-opt.svg" alt="Ahoy Coffee Co. logo"
-      class="h-16 w-16 mx-auto">
+    <a href="https://ahoy.coffee"><img src="@/assets/logo-outline-opt.svg"
+      alt="Ahoy Coffee Co. logo"
+      class="h-16 w-16 mx-auto"></a>
   </header>
 </template>
