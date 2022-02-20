1fd72ae5cddd8647b54c56e46ee6c58f0ef7935e
eddie cianci <defeated2k4@gmail.com>
2022-02-20 19:29:28 +0000

    upgrade dependendies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-const colors = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-  mode: 'jit',
-  purge: [
+  content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}'
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
