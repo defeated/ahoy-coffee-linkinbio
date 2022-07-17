8d3769bf6fb9cca67a128dc9eacd093375db6ed6
eddie cianci <defeated2k4@gmail.com>
2022-07-16 20:47:10 -0400

    add tailwind colors: cyan, sky

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        cyan: colors.cyan,
+        sky: colors.sky
+      }
+    },
 	},
 	plugins: [],
 }
